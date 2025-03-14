b57d6dfde773eaa64740b376d1c4e9fb42358f20
Pavel-Lopatin <workgamespace2020@gmail.com>
2025-03-14 12:33:04 +0500

    0.0.4
    - Added basic logic for BuildingPlacer

--- Assets/_project/Modules/BuildingSystem/Scripts/BuildingPlacer.cs
+++ Assets/_project/Modules/BuildingSystem/Scripts/BuildingPlacer.cs
@@ -1,10 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace BuldingSystem
 {
     public class BuildingPlacer : MonoBehaviour
     {
-        
+        [SerializeField] private List<GameObject> _placedGameObjects;
+
+        public void Init()
+        {
+            _placedGameObjects = new List<GameObject>();
+        }
+
+        public void PlaceBuilding(GameObject prefab, Vector3Int position)
+        {
+            var newBuilding = Instantiate(prefab, position, Quaternion.identity);
+            _placedGameObjects.Add(newBuilding);
+
+            Debug.Log("The building is built!");
+        }
+
+        public void DestroyBuilding(int buildingIndex)
+        {
+            Destroy(_placedGameObjects[buildingIndex]);
+            _placedGameObjects[buildingIndex] = null;
+        }
     }
 }
-
